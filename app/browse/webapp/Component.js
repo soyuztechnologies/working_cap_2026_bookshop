fd44c5d514db1a67ec04e8d42a1d5b2adba6a24a
Anubhav Oberoy <anubhav.abap@gmail.com>
2026-01-13 14:42:56 +0000

    stable bookshoop

---
+++ app/browse/webapp/Component.js
@@ -0,0 +1,15 @@
+// sap.ui.define(["sap/fe/core/AppComponent"], ac => ac.extend("bookshop.browse.Component", {
+//   metadata:{ manifest:'json' }
+// }))
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("bookshop.browse.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
